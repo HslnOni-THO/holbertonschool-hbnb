4ff67eef35acda462bde6f662f3f97c641d7560f
hslnoni-tho <9892@holbertonstudents.com>
2025-06-19 11:08:02 +0200

    add import models in api file

--- part2/hbnb/presentation/api.py
+++ part2/hbnb/presentation/api.py
@@ -8,3 +8,16 @@ def create_app():
         return jsonify({"message": "Welcome to HBnB API!"})
 
     return app
+
+from flask_restx import Namespace, Resource, fields
+from hbnb.services.facade import HBnBFacade  
+
+user_ns = Namespace('users', description='User operations')
+facade = HBnBFacade()
+
+user_model = user_ns.model('User', {
+    'id': fields.String(readonly=True, description='User ID'),
+    'email': fields.String(required=True, description='User email'),
+    'first_name': fields.String(description='First name'),
+    'last_name': fields.String(description='Last name'),
+})
