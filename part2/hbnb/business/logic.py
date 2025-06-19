04578285ce22a0564d8c72c88fe2e7a36c8c4afa
hslnoni-tho <9892@holbertonstudents.com>
2025-06-19 14:16:31 +0200

    add my structure and answer for the api and models files again

--- part2/hbnb/business/logic.py
+++
@@ -1,8 +0,0 @@
-from hbnb.persistence.repository import InMemoryRepository
-
-class BusinessLogic:
-    def __init__(self, repository):
-        self.repository = repository
-
-    def get_entity(self, entity_id):
-        return self.repository.get(entity_id)
